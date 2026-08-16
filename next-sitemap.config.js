7efba4f803dfd8620bee84b27869dbc46c35aa2f
piro0919 <piro.haniwa@gmail.com>
2026-08-17 03:31:05 +0900

    fix(seo): point the metadata at kkweb.io

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://siritori-timer.kk-web.link/",
+  siteUrl: process.env.SITE_URL || "https://siritori-timer.kkweb.io/",
   generateRobotsTxt: true,
 };
 
